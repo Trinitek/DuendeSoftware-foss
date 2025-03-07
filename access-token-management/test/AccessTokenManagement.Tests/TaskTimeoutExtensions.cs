5534b828ae306c08a790047c46b0a363f50ea905
Erwin van der Valk <erwin@vandervalk.pro>
2025-03-07 11:53:09 +0100

    little cleanup

---
+++ access-token-management/test/AccessTokenManagement.Tests/TaskTimeoutExtensions.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+
+namespace Duende.AccessTokenManagement.Tests;
+
+public static class TaskTimeoutExtensions
+{
+    private static TimeSpan IncreaseTimeoutIfDebuggerAttached(TimeSpan timeout)
+    {
+        // Wait a bit longer if the debugger is attached. This prevents timeouts during debugging.
+        if (Debugger.IsAttached) return TimeSpan.FromMinutes(10);
+
+        return timeout == default ? TimeSpan.FromSeconds(2) : timeout;
+    }
+
+    public static async Task ThrowOnTimeout(this Task task, TimeSpan timeout = default)
+    {
+        timeout = IncreaseTimeoutIfDebuggerAttached(timeout);
+
+        using (var cts = new CancellationTokenSource())
+        {
+            var delayTask = Task.Delay(timeout, cts.Token);
+
+            var resultTask = await Task.WhenAny(task, delayTask);
+            if (resultTask == delayTask)
+                // Operation cancelled
+                throw new OperationCanceledException();
+            cts.Cancel();
+
+            await task;
+        }
+    }
+}
